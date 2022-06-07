8223a237b1fda98ed75e317ee0d55065c83c0db1
Ricepaste <n22126@gmail.com>
2022-06-07 17:06:27 +0800

    Can test the consumption and voltage now

--- controller.ino
+++ controller.ino
@@ -11,8 +11,12 @@ void setup()
 
 // the loop function runs over and over again forever
 String incomingByte = "";
+double ap;
+double voltage;
+bool Solar_Mode = false;
 void loop()
 {
+
     if (Serial.available() > 0)
     {
         // read the incoming byte:
@@ -26,12 +30,25 @@ void loop()
             digitalWrite(LED_BUILTIN, HIGH);
             digitalWrite(12, HIGH);
             Serial.println("solar_mode is ON");
+            Solar_Mode = true;
         }
         else if (incomingByte == "normal\n")
         {
             digitalWrite(LED_BUILTIN, LOW);
             digitalWrite(12, LOW);
             Serial.println("normal electricity mode is ON");
+            Solar_Mode = false;
         }
     }
+    Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    ap = analogRead(A0);
+    ap = (ap - 505.) * 0.02;
+    Serial.println("LED current consumption: " + String(ap) + "A");
+    ap = analogRead(A1);
+    ap = (ap - 505.) * 0.02;
+    Serial.println("USB hub current consumption: " + String(ap) + "A");
+    voltage = analogRead(A2) / 68 * 1.67;
+    Serial.println("solar panel voltage : " + String(voltage) + "V");
+
+    delay(1000);
 }
